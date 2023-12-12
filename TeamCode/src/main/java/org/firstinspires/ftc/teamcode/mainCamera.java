61cae93f039c700edcea263aa811aff1fb927198
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2023-12-12 15:54:43 -0800

    erlis camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainCamera.java
@@ -22,7 +22,7 @@ public class mainCamera extends LinearOpMode {
     }
 
     //Detecting if there is a team prop
-    public int propDetection() {
+    public void propDetection() {
         boolean propIsDetected = false;
         List<Recognition> currentRecognitions = tfod.getRecognitions(); // List of recognitions
         for (Recognition recognition : currentRecognitions) {
