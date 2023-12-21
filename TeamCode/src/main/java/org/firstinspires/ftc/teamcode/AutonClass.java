4847f382646d5795b76328b2c79f16d8661b5001
Logan Rembisz <logan.rem070@gmail.com>
2023-12-21 11:51:44 -0500

    Update AutonClass.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClass.java
@@ -15,31 +15,12 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 
 public class AutonClass extends LinearOpMode {
-    /* Declare OpMode members. */
-
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
-    private DcMotor rightArmMotor = null;
-    private DcMotor leftArmMotor = null;
 
     private ElapsedTime runtime = new ElapsedTime();
 
     public SampleMecanumDrive drive = null;
 
-    // Calculate the COUNTS_PER_INCH for your specific drive train.
-    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
-    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
-    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
-    // This is gearing DOWN for less speed and more torque.
-    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
-    final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
-    final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
-    final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
-    final double DRIVE_SPEED = 0.6;
-    final double TURN_SPEED = 0.5;
+    public int spikeMark = 0;
 
     //HardwareMap yes;
     public AutonClass(HardwareMap no) {
@@ -65,6 +46,14 @@ public class AutonClass extends LinearOpMode {
 
     public void boardPlace() {
         telemetry.addData("j", "h");
+    }
+
+    public void getSpikeMark() {
+        EOCV_Pipe1 pipe = new EOCV_Pipe1(hardwareMap, telemetry);
+        while (spikeMark == 0 || spikeMark == 4 || !isStopRequested()) {
+            spikeMark = pipe.getFinal_Result();
+            sleep(10);
+        }
     }
 
     //Basic RR Functions for all classes to use.
@@ -108,27 +97,27 @@ public class AutonClass extends LinearOpMode {
 
 
 
-        /* Initialize the drive system variables.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-        rightArmMotor = hardwareMap.get(DcMotor.class, "arm_motor_right");
-        leftArmMotor = hardwareMap.get(DcMotor.class, "arm_motor_left");
+/* Initialize the drive system variables.
+leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+rightArmMotor = hardwareMap.get(DcMotor.class, "arm_motor_right");
+leftArmMotor = hardwareMap.get(DcMotor.class, "arm_motor_left");
 
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightArmMotor.setDirection(DcMotor.Direction.FORWARD);
-        leftArmMotor.setDirection(DcMotor.Direction.REVERSE);
+// To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+// When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
+// Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+rightArmMotor.setDirection(DcMotor.Direction.FORWARD);
+leftArmMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        leftArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+leftArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+rightArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+leftArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+rightArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 */
