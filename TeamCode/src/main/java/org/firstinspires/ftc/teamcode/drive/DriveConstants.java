708adaffe2f7ccc9080a1a257231cd0c757d3b14
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2024-01-29 16:03:06 -0500

    strafe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -56,7 +56,7 @@ public class DriveConstants {
      */
     public static double kV = 0.01859;
     public static double kA = 0.004;
-    public static double kStatic = 0.0   ;
+    public static double kStatic = 0.0;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
