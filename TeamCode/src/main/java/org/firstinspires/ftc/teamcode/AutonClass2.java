e5f25ed10b02b7d55ad7fe9520af7e0b2b757e25
AdamJacuch <adamjacuch@gmail.com>
2024-02-03 20:17:57 -0500

    Update AutonClass2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClass2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClass2.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -119,29 +120,70 @@ public class AutonClass2 extends LinearOpMode {
                     .build();
         //Red Near Center
         } else if (redAlliance && !isFar && redMark == 2) {
-            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d())
-                    .back(3)
+            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d(10, -60, Math.toRadians(-90)))
+                    .strafeLeft(20)
+                    .strafeTo(new Vector2d(40, -23))
+                    .turn(Math.toRadians(-90))
+                    .forward(20)
+                    .back(20)
+                    .strafeTo(new Vector2d(42, -34))
+                    .strafeRight(25)
+                    .back(16)
                     .build();
         //Red Near Right
         } else if (redAlliance && !isFar && redMark == 3) {
-            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d())
-                    .back(3)
+            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d(10, -60, Math.toRadians(-90)))
+                    .setReversed(false)
+                    .strafeTo(new Vector2d(25, -32))
+                    .turn(Math.toRadians(-90))
+                    .forward(20)
+                    .waitSeconds(0.2)
+                    .strafeTo(new Vector2d(50, -28))
                     .build();
 
         //Red Far Left
         } else if (redAlliance && isFar && redMark == 1) {
-            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d())
-                    .back(3)
+            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(-90)))
+                    .strafeTo(new Vector2d(-47, -50))
+                    .turn(Math.toRadians(-90))
+                    .strafeRight(30)
+                    .strafeLeft(10)
+                    .back(8)
+                    .waitSeconds(0.2)
+                    .back(7)
+                    .strafeRight(25)
+                    .back(75)
+                    .strafeTo(new Vector2d(42, -29))
+                    .strafeRight(20)
+                    .back(16)
                     .build();
         //Red Far Center
         } else if (redAlliance && isFar && redMark == 2) {
-            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d())
-                    .back(3)
+            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(-90)))
+                    .back(65)
+                    .forward(22)
+                    .waitSeconds(0.2)
+                    .back(12)
+                    .turn(Math.toRadians(-90))
+                    .back(75)
+                    .strafeTo(new Vector2d(42, -34))
+                    .strafeRight(25)
+                    .back(16)
                     .build();
         //Red Far Right
         } else if (redAlliance && isFar && redMark == 3) {
-            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d())
-                    .back(3)
+            trajMoveU = drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(-90)))
+                    .strafeTo(new Vector2d(-50, -30))
+                    .turn(Math.toRadians(90))
+                    .forward(18)
+                    .waitSeconds(0.2)
+                    .back(15)
+                    .strafeLeft(20)
+                    .turn(Math.toRadians(180))
+                    .back(65)
+                    .strafeTo(new Vector2d(42, -40))
+                    .strafeRight(31)
+                    .back(16)
                     .build();
 
         //Blue Near Left
